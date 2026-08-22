774be927666b6898ce288e85b4a6dc5b1948f42d
16E121 <gokulsabari.k@gmail.com>
2026-08-22 11:01:59 +0530

    pdf attachement throw whatsapp

--- routes/masters.mjs
+++ routes/masters.mjs
@@ -433,4 +433,8 @@ MastersRouter.post('/productUnitConversion', productUnitConversion.postConversio
 MastersRouter.put('/productUnitConversion', productUnitConversion.putConversion);
 MastersRouter.delete('/productUnitConversion', productUnitConversion.deleteConversion);
 
+
+
+MastersRouter.post('/whatsapp/uploadPendingBillsPdf', whatsapp.postPendingBillsPdf);
+MastersRouter.delete('/whatsapp/cleanupPdf', whatsapp.whatsappDelete);
 export default MastersRouter;
